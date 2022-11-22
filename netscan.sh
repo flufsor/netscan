af28fd10f9f04b4303b667b41888847c9c28ed56
Tom Goedemé <flufsor@gmail.com>
2022-11-22 22:54:59 +0100

    Add Tcpdump for vlans

--- netscan.sh
+++ netscan.sh
@@ -1,7 +1,39 @@
-#! /bin/sh
+#!/bin/bash
 
-VLAN_MIN=1
-VLAN_MAX=4094
+DUMP_AMOUNT=20
+
+function request_dhcp_address()
+{
+    echo "Requesting DHCP address for interface $interface"
+    dhclient -v $interface
+
+    # Remove old DHCP lease
+    echo "Removing old DHCP lease"
+    dhclient -r $interface
+
+    # Request ip address from DHCP server
+    echo "Requesting ip address from DHCP server"
+    dhclient -4 $interface -v
+
+    # Check for successful DHCP request
+    ip=$(ip addr show $interface | grep "inet " | awk '{print $2}' | cut -d/ -f1)
+
+    if [ -z "$ip" ]; then
+      echo "Could not get ip address from DHCP server"
+      exit 1
+    else
+      gateway=$(ip route show 0.0.0.0/0 dev $interface | cut -d\  -f3)
+      echo "Ip: $ip"
+      echo "Gateway: $gateway"
+    fi
+
+    # Ping test gateway
+    if ping -q -c 1 -W 1 "$gateway" >/dev/null; then
+      echo "IPv4 is up"
+    else
+      echo "IPv4 is down"
+    fi
+}
 
 # Check if the user is root
 if [ $(id -u) -ne 0 ]; then
@@ -14,7 +46,7 @@ if [ -z "$1" ]; then
   echo "Please provide an interface"
   exit 1
 else
-  if [ $(ip addr | grep -w $1 | wc -l) -eq 0 ]; then
+  if [ $(ip addr | grep -c $1) -eq 0 ]; then
     echo "Please provide a valid interface"
     exit 1
   fi
@@ -34,38 +66,19 @@ if [ $(cat /sys/class/net/$interface/operstate) = "down" ]; then
   echo boo
 fi
 
-# Remove old DHCP lease
-echo "Removing old DHCP lease"
-dhclient -r $interface
+# TODO: Fix infinite waiting for tcpdump
+# Scan for VLANs
+tcpdump -nn -e vlan -i $interface -C $DUMP_AMOUNT > scans/tcpdump-$interface.log 2> /dev/null
+mapfile -t result < <(cat scans/tcpdump-$interface.log  | grep -oP '(?:vlan )([0-9])+' | awk '{print $2}' | sort -nu)
 
-# Request ip address from DHCP server
-echo "Requesting ip address from DHCP server"
-dhclient -4 $interface -v
-
-# Check for successful DHCP request
-ip=$(ip addr show $interface | grep "inet " | awk '{print $2}' | cut -d/ -f1)
-
-if [ -z "$ip" ]; then
-  echo "Could not get ip address from DHCP server"
-  exit 1
+if [ ${#result[@]} -ne 0 ]; then
+  echo "VLANs found:"
+  for vlan in "${result[@]}"
+  do
+    echo -e "\t -VLAN: $vlan"
+  done
 else
-  gateway=$(ip route show 0.0.0.0/0 dev $interface | cut -d\  -f3)
-  echo "Ip: $ip"
-  echo "Gateway: $gateway"
-fi
-
-# Ping test gateway
-if ping -q -c 1 -W 1 $gateway >/dev/null; then
-  echo "IPv4 is up"
-else
-  echo "IPv4 is down"
-fi
-
-# Ping test google.com
-if ping -q -c 1 -W 1 www.google.com >/dev/null; then
-  echo "Internet connectivity is up"
-else
-  echo "Internet connectivity is down"
+  echo "No VLANs found"
 fi
 
 # Restore Files to original state
