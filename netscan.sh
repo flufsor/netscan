d6da1c9d4408552f91bf4f26f84e91e54b520512
Tom Goedemé <flufsor@gmail.com>
2022-12-03 16:10:28 +0100

    Add finding static ip

--- netscan.sh
+++ netscan.sh
@@ -2,10 +2,12 @@
 # title         : netscan.sh
 # description   : A Network port scanner for linux.
 # author        : Tom Goedemé https://github.com/flufsor
-# usage         : ./netscan.sh [interface]
+# usage         : ./netscan.sh -t <interface> [-i <vlan scan time>]
 # ======================================================================================================================
 
 TCPDUMP_TIMEOUT=60
+IPFILTER='((192|172|10){1,3}\.){1}([0-9]{1,3}\.){2}(?!255)([0-9]{1,3})'
+PREFIXFILTER='((192|172|10){1,3}\.){1}([0-9]{1,3}\.){2}'
 
 function test_connection()
 {
@@ -18,7 +20,7 @@ function test_connection()
 }
 
 # Check if the user is root
-if [ $(id -u) -ne 0 ]; then
+if [ "$(id -u)" -ne 0 ]; then
   echo "Please run as root"
   exit 1
 fi
@@ -31,45 +33,44 @@ while getopts ':i:t:h' opt; do
         exit 1
       fi
       INTERFACE=$OPTARG
-      INTERFACESHORT=$(echo $INTERFACE | cut -c -6) # Some interfaces are too long
+      INTERFACESHORT=$(echo "$INTERFACE" | cut -c -6) # Some interfaces are too long
       ;;
 
     t)
       TCPDUMP_TIMEOUT=$OPTARG
       ;;
-
     h)
-      Usage: $(basename $0) -t <interface> [-i <vlan scan time>]
+      echo "Usage: $(basename "$0") -t <interface> [-i <vlan scan time>]"
       exit 0
       ;;
 
     *)
-      echo -e "Invalid command option.\nUsage: $(basename $0) -i <interface> [-t <vlan scan time>]"
+      echo -e "Invalid command option.\nUsage: $(basename "$0") -i <interface> [-t <vlan scan time>]"
       exit 1
       ;;
   esac
 done
-shift "$(($OPTIND -1))"
+shift "$(("$OPTIND" -1))"
 
 if [ -z "$INTERFACE" ]; then
-  echo -e "Invalid command option.\nUsage: $(basename $0) -i <interface>"
+  echo -e "Invalid command option.\nUsage: $(basename "$0") -i <interface>"
   exit 1
 fi
 
 echo "Starting Netscan on $INTERFACE"
 
 # Set interface up
-if [ $(cat /sys/class/net/$INTERFACE/operstate) = "down" ]; then
+if [ $(cat "/sys/class/net/$INTERFACE/operstate") = "down" ]; then
   echo "Setting $INTERFACE up"
-  ip link set $INTERFACE up
+  ip link set "$INTERFACE" up
 else 
   echo "Interface $INTERFACE is already up"
 fi
 
 # Scan for VLANs
 echo "Starting VLAN scan for $TCPDUMP_TIMEOUT seconds"
-timeout $TCPDUMP_TIMEOUT tcpdump -nnt -e vlan -i $INTERFACE > scans/tcpdump-$INTERFACE.log 2>/dev/null
-mapfile -t vlans < <(cat scans/tcpdump-$INTERFACE.log  | grep -oP '(?:vlan )([0-9])+' | awk '{print $2}' | sort -nu)
+timeout "$TCPDUMP_TIMEOUT" tcpdump -nnt -e vlan -i "$INTERFACE" > "scans/tcpdump-$INTERFACE.log" 2>/dev/null
+mapfile -t vlans < <(grep -oP '(?:vlan )([0-9])+' "scans/tcpdump-$INTERFACE.log"  | awk '{print $2}' | sort -nu)
 
 if [ ${#vlans[@]} -ne 0 ]; then
   echo "VLANs found:"
@@ -78,31 +79,53 @@ if [ ${#vlans[@]} -ne 0 ]; then
     echo -e "\t-VLAN: $vlan"
   done
 
+  echo -e "\nRequesting IP addresses for VLANs"
   for vlan in "${vlans[@]}"; do
     echo -e "Scanning Vlan: $vlan"
 
     # Set interface up
-    ip link add link $INTERFACE name $INTERFACESHORT.$vlan type vlan id $vlan >/dev/null 2>&1
+    ip link add link "$INTERFACE" name "$INTERFACESHORT.$vlan" type vlan id "$vlan" >/dev/null 2>&1
     # Requst DHCP address
-    timeout 2 dhclient -cf ./dhclient.conf -d -1 $INTERFACESHORT.$vlan &>/dev/null
+    timeout 2 dhclient -cf ./dhclient.conf -d -1 "$INTERFACESHORT.$vlan" &>/dev/null
     # Check for successful DHCP request
-    ip=$(ip addr show $INTERFACESHORT.$vlan | grep "inet " | awk '{print $2}')
+    ip=$(ip addr show "$INTERFACESHORT.$vlan" | grep "inet " | awk '{print $2}')
 
     if [ -z "$ip" ]; then
       echo -e "\tCould not get ip address from DHCP server"
-      echo -e "\tRunning manual IP scan"
+      echo -e "\tTrying to find manual address"
+
+      # Trying to find a manual address
+      FOUND_HOSTS=$(grep "vlan $vlan" "scans/tcpdump-ens19.log" | grep -Po "$IPFILTER" | sort -u)
+      PREFIX=$(echo "$FOUND_HOSTS" | grep -Po "$PREFIXFILTER" | sort -u)
+      FIRST_HOST=$(echo "$FOUND_HOSTS" | head -n 1)
+
+      for IP_OCTET in {20..234}; do
+        if echo "$PREFIX$IP_OCTET" | grep -qv "$FOUND_HOSTS"; then
+          IP="$PREFIX$IP_OCTET"
+          for NETMASK in {24,16,8}; do
+            ip addr add "$IP/$NETMASK" dev "$INTERFACESHORT.$vlan"
+            ip link set "$INTERFACESHORT.$vlan" up
+            # Test connection
+            if ping -c 1 "$FIRST_HOST" >/dev/null 2>&1; then
+              echo -e "\tFound manual address: $IP/$NETMASK"
+              break
+            fi
+          done
+          break
+        fi
+      done
     else
       echo -e "\tGot DHCP ip: $ip"
     fi
   done
 else
   echo "No VLANs found, trying native VLAN"
-    timeout 2 dhclient -cf ./dhclient.conf -d -1 $INTERFACE &>/dev/null
+    timeout 2 dhclient -cf ./dhclient.conf -d -1 "$INTERFACE" &>/dev/null
 fi
 
 # Cleaning up vlan interfaces
-echo "Cleaning Up"
+echo -e "\nCleaning Up"
 
 for vlan in "${vlans[@]}"; do
-  ip link del link dev $INTERFACESHORT.$vlan
+  ip link del link dev "$INTERFACESHORT.$vlan"
 done
