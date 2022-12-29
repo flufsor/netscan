f4bb0edcdc46a034f2bda75fd7be17f6afcfbf84
Tom Goedemé <flufsor@gmail.com>
2022-12-29 01:12:14 +0100

    Add l

--- netscan.sh
+++ netscan.sh
@@ -15,6 +15,7 @@ function ShowHelp()
       echo "Options:"
       echo "  -c  Cleanup after scan"
       echo "  -i  Interface to scan"
+      echo "  -l  scan for LLDP hosts"
       echo "  -t  Timeout for tcpdump"
       echo "  -h  Show this help"
 }
@@ -25,7 +26,7 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-while getopts ':ci:t:h' opt; do
+while getopts ':cli:t:h' opt; do
   case "$opt" in
     c)
       CLEANUP=true;
@@ -35,10 +36,12 @@ while getopts ':ci:t:h' opt; do
         echo "Please provide a valid interface"
         exit 1
       fi
-      INTERFACE=$OPTARG
-      INTERFACESHORT=$(echo "$INTERFACE" | cut -c -6) # Some interfaces are too long
+      interface=$OPTARG
+      interfaceShort=$(echo "$interface" | cut -c -6) # Some interfaces are too long
       ;;
-
+    l)
+      LLDP=true;
+      ;;
     t)
       TCPDUMP_TIMEOUT=$OPTARG
       ;;
@@ -55,50 +58,50 @@ while getopts ':ci:t:h' opt; do
 done
 shift "$(("$OPTIND" -1))"
 
-if [ -z "$INTERFACE" ]; then
+if [ -z "$interface" ]; then
   echo -e "Invalid command option.\nUsage: $(basename "$0") -i <interface>"
   exit 1
 fi
 
-echo "Starting Netscan on $INTERFACE"
+echo "Starting Netscan on $interface"
 
 # Set interface up
-if [ $(cat "/sys/class/net/$INTERFACE/operstate") = "down" ]; then
-  echo "Setting $INTERFACE up"
-  ip link set "$INTERFACE" up
+if [ "$(cat "/sys/class/net/$interface/operstate")" = "down" ]; then
+  echo "Setting $interface up"
+  ip link set "$interface" up
 else 
-  echo "Interface $INTERFACE is already up"
+  echo "interface $interface is already up"
 fi
 
-# Scan for VLANs
+# Scan for vlans
 echo "Starting VLAN scan for $TCPDUMP_TIMEOUT seconds"
-timeout "$TCPDUMP_TIMEOUT" tcpdump -nnt -e vlan -i "$INTERFACE" > "scans/tcpdump-$INTERFACE.log" 2>/dev/null
-mapfile -t vlans < <(grep -oP '(?:vlan )([0-9])+' "scans/tcpdump-$INTERFACE.log"  | awk '{print $2}' | sort -nu)
+timeout "$TCPDUMP_TIMEOUT" tcpdump -nnt -e vlan -i "$interface" > "scans/tcpdump-$interface.log" 2>/dev/null
+mapfile -t vlans < <(grep -oP '(?:vlan )([0-9])+' "scans/tcpdump-$interface.log"  | awk '{print $2}' | sort -nu)
 
-if [ ${#vlans[@]} -ne 0 ]; then
-  echo "VLANs found:"
+if [ ${#vlans[@]} -ne 0 ]; then # Check if there are any vlans
+  echo "vlans found:"
 
   for vlan in "${vlans[@]}"; do
     echo -e "\t-VLAN: $vlan"
   done
 
-  echo -e "\nRequesting IP addresses for VLANs"
+  echo -e "\nRequesting IP addresses for vlans"
   for vlan in "${vlans[@]}"; do
     echo -e "Scanning Vlan: $vlan"
 
     # Set interface up
-    ip link add link "$INTERFACE" name "$INTERFACESHORT.$vlan" type vlan id "$vlan" >/dev/null 2>&1
+    ip link add link "$interface" name "$interfaceShort.$vlan" type vlan id "$vlan" >/dev/null 2>&1
     # Requst DHCP address
-    timeout 2 dhclient -cf ./dhclient.conf -d -1 "$INTERFACESHORT.$vlan" &>/dev/null
+    timeout 2 dhclient -cf ./dhclient.conf -d -1 "$interfaceShort.$vlan" &>/dev/null
     # Check for successful DHCP request
-    ip=$(ip addr show "$INTERFACESHORT.$vlan" | grep "inet " | awk '{print $2}')
+    ip=$(ip addr show "$interfaceShort.$vlan" | grep "inet " | awk '{print $2}')
 
     if [ -z "$ip" ]; then
       echo -e "\tCould not get ip address from DHCP server"
       echo -e "\tTrying to find manual address"
 
       # Trying to find a manual address
-      FOUND_HOSTS=$(grep "vlan $vlan" "scans/tcpdump-$INTERFACE.log" | grep -Po "$IPFILTER" | sort -u)
+      FOUND_HOSTS=$(grep "vlan $vlan" "scans/tcpdump-$interface.log" | grep -Po "$IPFILTER" | sort -u)
       PREFIX=$(echo "$FOUND_HOSTS" | grep -Po "$PREFIXFILTER" | sort -u)
       FIRST_HOST=$(echo "$FOUND_HOSTS" | head -n 1)
 
@@ -106,8 +109,8 @@ if [ ${#vlans[@]} -ne 0 ]; then
         if echo "$PREFIX$IP_OCTET" | grep -qv "$FOUND_HOSTS"; then
           IP="$PREFIX$IP_OCTET"
           for NETMASK in {24,16,8}; do
-            ip addr add "$IP/$NETMASK" dev "$INTERFACESHORT.$vlan"
-            ip link set "$INTERFACESHORT.$vlan" up
+            ip addr add "$IP/$NETMASK" dev "$interfaceShort.$vlan"
+            ip link set "$interfaceShort.$vlan" up
             # Test connection
             if ping -c 1 "$FIRST_HOST" >/dev/null 2>&1; then
               echo -e "\tFound manual address: $IP/$NETMASK"
@@ -117,20 +120,51 @@ if [ ${#vlans[@]} -ne 0 ]; then
           break
         fi
       done
+      echo -e "\tCould not find manual address"
     else
       echo -e "\tGot DHCP ip: $ip"
     fi
+
+    if [ "$LLDP" == true ]; then      # Scan for LLDP hosts
+      echo -e "\tScanning for LLDP hosts"
+      lldpcli show neighbors ports "$interfaceShort.$vlan" > "scans/lldp-$interfaceShort.$vlan.log"
+      mapfile -t lldp_neighbors < "scans/lldp-$interfaceShort.$vlan.log"
+
+      for line in "${lldp_neighbors[@]}"; do
+        mac=$(echo "$line" | grep -oP 'ChassisID: mac \K.+')
+        ip=$(echo "$line" | grep -oP 'MgmtIP: \K.+')
+
+        [[ -n $mac ]] && [[ -n $ip ]] && echo -e "\t\t$mac $ip"
+      done
+    fi
+
   done
-else
-  echo "No VLANs found, trying native VLAN"
-    timeout 2 dhclient -cf ./dhclient.conf -d -1 "$INTERFACE" &>/dev/null
+else # No vlans found
+  echo "No vlans found, trying native VLAN"
+  timeout 2 dhclient -cf ./dhclient.conf -d -1 "$interface" &>/dev/null
+  ip=$(ip addr show "$interface" | grep "inet " | awk '{print $2}')
+  #TODO: Check if ip is empty and try static ip
+
+  if [ "$LLDP" == true ]; then      # Scan for LLDP hosts
+    echo -e "\tScanning for LLDP hosts"
+    lldpcli show neighbors ports "$interface" > "scans/lldp-$interface.log"
+    mapfile -t lldp_neighbors < "scans/lldp-$interface.log"
+    for line in "${lldp_neighbors[@]}"; do
+      mac=$(echo "$line" | grep -oP 'ChassisID: mac \K.+')
+      ip=$(echo "$line" | grep -oP 'MgmtIP: \K.+')
+
+      [[ -n $mac ]] && [[ -n $ip ]] && echo -e "\t\t$mac $ip"
+    done
+
+  fi
 fi
 
-
-if [ $CLEANUP == true ]; then
+if [ "$CLEANUP" == true ]; then
   echo "Cleaning up"
-  ip link set "$INTERFACE" down
-  for vlan in "${vlans[@]}"; do
-    ip link delete "$INTERFACESHORT.$vlan"
-  done
+  ip link set "$interface" down
+  if [ ${#vlans[@]} -ne 0 ]; then
+    for vlan in "${vlans[@]}"; do
+      ip link delete "$interfaceShort.$vlan"
+    done
+  fi
 fi
